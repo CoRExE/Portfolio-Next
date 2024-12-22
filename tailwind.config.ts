44c819cc17fa0636afd581a814357371678f327e
ceestos <jules.langagne@gmail.com>
2024-12-22 11:16:06 +0100

    CSS Theme updated + Adding dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -9,10 +10,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'base-900': '#1a1a1a',
+        'base-800': '#2a2a2a',
+        'base-700': '#3a3a3a',
+        'base-content': '#ffffff',
+        'primary': '#ff5722',
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 } satisfies Config;
